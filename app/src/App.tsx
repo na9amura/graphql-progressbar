e573d6f519f942a0d52795f73c0b853a29dc9e31
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2019-10-22 13:37:16 +0900

    add page layer to divide global level config and page level config

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,71 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { ProgressBar } from './components/ProgressBar';
-import styled from 'styled-components';
-import { Button } from './components/Button';
-import { changeProgress, getProgress } from './repositories/ProgressRepository'
+import React from 'react';
+import { Progress } from './pages/Progress';
 
-const Frame = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  > * ~ * {
-    margin-top: 1rem;
-  }
-`
-
-const interval = 500
-
-const App: React.FC = () => {
-  const [started, updateStarted] = useState<boolean>(false)
-  const [progress, updateProgress] = useState<number>(0)
-
-  const onClick = async (started: boolean) => {
-    if (started) {
-      await changeProgress('cancel')
-      updateStarted(false)
-      updateProgress(0)
-    } else {
-      const res = await changeProgress('start')
-      updateStarted(res)
-    }
-  }
-
-  useEffect(() => {
-    const update = async () => {
-      const current = await getProgress()
-      updateProgress(current)
-      if (current >= 1) updateStarted(false)
-    }
-
-    const startUpdate = () => {
-      if (started) {
-        update()
-        return setInterval(update, interval)
-      }
-    }
-
-    const id: number | undefined = startUpdate()
-
-    return () => {
-      if (id) {
-        clearInterval(id)
-        changeProgress('cancel')
-      }
-    }
-  }, [started])
-
+export const App: React.FC = () => {
   return (
     <div className="App">
-      <Frame>
-        <ProgressBar progress={progress} interval={interval} />
-        <Button onClick={() => onClick(started)}>
-          {started ? 'Cancel' : 'Start'}
-        </Button>
-      </Frame>
+      <Progress/>
     </div>
-  );
+  )
 }
-
-export default App;
